814a1b3137b28f081fea8ade751deda9e130d3f4
amzan1 <amzanabdullah97@gmail.com>
2023-08-10 16:27:58 +0300

    v.1

--- main.js
+++ main.js
@@ -22,20 +22,61 @@ postButton.addEventListener('click', () => {
 
 // LogIn
 
-function LogIn(params) {
+function LogIn() {
     let name = document.getElementById("name")
-    let pass = document.getElementById("pass")
+    let pass = document.getElementById("pass").value
     let showName = document.getElementById("user-name")
+    
 
     showName.innerText = "hello " + name.value
     localStorage.setItem("name", name.value)
-    location.href = "./bloog.html"
-
-
+    // 
+    // check name
+    if(name.value == "") {  
+        document.getElementById("message").innerHTML = "**Enter your name please!";  
+        return false;  
+    }  
+    if(name.value.length <=5 ) {  
+        document.getElementById("message").innerHTML = "**Name length must be atleast 5 characters";  
+        return false;  
+    }  
+    // check passwoerd
+    if(pass == "") {  
+        document.getElementById("message").innerHTML = "**Fill the password please!";  
+        return false;  
+    }  
+    if(pass.length < 8) {  
+        document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+        return false;  
+    } 
+    
+    location.href = "./bloog.html";
+        
+    
+    
+    
+    
 }
 
 
 // Bloog
+function logout(){
+    localStorage.removeItem("name")
+    window.location.href="index.html"
+    
+
+}
+
+    let name1 = document.getElementById("name")
+    let UserName =  localStorage.getItem("name")
+    if(name1 == UserName){
+        window.location.href="bloog.html"
+        
+    }
+
+else if(name1 != UserName){
+    window.location.href="index.html"
+}
 
 let userName = document.getElementById("user-name")
 userName.innerText = localStorage.getItem("name")
